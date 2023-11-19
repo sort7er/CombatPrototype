7f51e4cebe248a5400d20016de01c845705c4600
sort7er <98805454+sort7er@users.noreply.github.com>
2023-11-19 02:55:41 +0100

    Continued on the archetypeprefab

--- Assets/_Scripts/Defenitions/ArchetypePrefab.cs
+++ Assets/_Scripts/Defenitions/ArchetypePrefab.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ArchetypePrefab : MonoBehaviour
 {
     public Animator archetypeAnim { get; private set; }
+
+
+    private enum AttackType
+    {
+        light,
+        heavy,
+        unique
+    }
+
+    private List<AttackType> attackQueue = new();
+
+    private int queueCapasity = 1;
 
     private void Awake()
     {
@@ -11,13 +24,56 @@ public class ArchetypePrefab : MonoBehaviour
 
     public void Fire()
     {
-        archetypeAnim.SetTrigger("Fire");
-        Debug.Log("Huh");
+        CheckAttack(AttackType.light);
     }
     public void HeavyFire()
     {
-        archetypeAnim.SetTrigger("HeavyFire");
+        CheckAttack(AttackType.heavy);
     }
+    
+    private void CheckAttack(AttackType attackType)
+    {
+        if(attackQueue.Count > 0)
+        {
+            //Add to queue
+        }
+        else
+        {
+            Attack(attackType);
+        }
+    }
+
+    private void AddToQueue(AttackType attackType)
+    {
+        //if(attackQueue.Count < queueCapasity)
+    }
+
+    public void CheckQueue()
+    {
+        // Attack if queue is not empty
+        if(attackQueue.Count > 0)
+        {
+            Attack(attackQueue[0]);
+            attackQueue.RemoveAt(0);
+        }
+    }
+
+    private void Attack(AttackType attackType)
+    {
+        if(attackType == AttackType.light)
+        {
+            archetypeAnim.SetTrigger("Fire");
+        }
+        else if(attackType == AttackType.heavy)
+        {
+            archetypeAnim.SetTrigger("HeavyFire");
+        }
+        else if (attackType == AttackType.unique)
+        {
+            Debug.Log("Unique");
+        }
+    }
+
     public void StopFire()
     {
         archetypeAnim.ResetTrigger("Fire");
